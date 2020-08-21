299402b2123b426a08f47107a319d0acce872079
andrey18106 <andrey18106x@gmail.com>
2020-08-21 17:28:33 +0300

    Minor fixes

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,7 +3,7 @@ window.onload = () => {
     let tabs = document.getElementsByClassName('tab');
     let tab_controls = document.getElementsByClassName('footer-menu-item');
 
-    Array.from(tab_controls).map(tab_control => {
+    Array.from(tab_controls).forEach(tab_control => {
         tab_control.addEventListener('click', event => {
             let selected_tab = document.getElementById('tab-' + tab_control.getAttribute('data-tab-id'));
 
@@ -11,8 +11,8 @@ window.onload = () => {
                 tab.classList.remove('tab-active');
             });
 
-            Array.from(tab_controls).forEach(tab_control => {
-                tab_control.classList.remove('footer-menu-item__active');
+            Array.from(tab_controls).forEach(tab_contr => {
+                tab_contr.classList.remove('footer-menu-item__active');
             });
 
             tab_control.classList.add('footer-menu-item__active')
