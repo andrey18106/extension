11b30446a96f1727298676a4a2871f502aeb0eeb
andrey18106 <andrey18106x@gmail.com>
2020-08-21 19:27:27 +0300

    Switched forEach to forOf

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,21 +3,21 @@ window.onload = () => {
     let tabs = document.getElementsByClassName('tab');
     let tab_controls = document.getElementsByClassName('footer-menu-item');
 
-    Array.from(tab_controls).forEach(tab_control => {
+    for (let tab_control of tab_controls) {
         tab_control.addEventListener('click', event => {
             let selected_tab = document.getElementById('tab-' + tab_control.getAttribute('data-tab-id'));
 
-            Array.from(tabs).forEach(tab => {
+            for (let tab of tabs) {
                 tab.classList.remove('tab-active');
-            });
+            }
 
-            Array.from(tab_controls).forEach(tab_contr => {
+            for (let tab_contr of tab_controls) {
                 tab_contr.classList.remove('footer-menu-item__active');
-            });
+            }
 
             tab_control.classList.add('footer-menu-item__active')
             selected_tab.classList.add('tab-active');
         });
-    })
+    }
 
 }
